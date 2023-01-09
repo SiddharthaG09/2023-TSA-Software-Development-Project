f76ee917e7b310ca94693e31a1fe4a4f6e8359d6
Siddhartha Guntupalli <siddu@guntupalli.net>
2023-01-08 22:48:10 -0600

    added a basic model that uses the webcam and recognizes your face along with the xml file

--- Python/main.py
+++ Python/main.py
@@ -1,39 +1,36 @@
 import cv2
-import numpy as np
-import tensorflow as tf
 
-# Load the trained model
-model = tf.keras.models.load_model("model.h5")
-
-# Define the labels
-labels = {0: "Happy", 1: "Sad"}
+# Load the Haar cascade for face detection
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Start the webcam
 cap = cv2.VideoCapture(0)
 
 while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    if not ret:
-        break
+    # Read the frame from the webcam
+    _, frame = cap.read()
 
-    # Pre-process the frame for the model
-    frame = cv2.resize(frame, (48, 48))
-    frame = np.expand_dims(frame, axis=0)
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Predict the emotion
-    prediction = model.predict(frame)[0]
-    label = labels[np.argmax(prediction)]
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-    # Display the frame with the label
-    cv2.putText(frame, label, (10, 50),
-                cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
-    cv2.imshow("Frame", frame)
+    # Draw a box around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.putText(frame, "Face", (x, y-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
 
-    # Break the loop if the user presses "q"
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    # Display the resulting frame
+    cv2.imshow('Webcam', frame)
+
+    # Stop the program if the user presses 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Release the webcam
 cap.release()
+
+# Destroy all windows
 cv2.destroyAllWindows()
